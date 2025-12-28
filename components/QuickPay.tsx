135c452bb3c0081037e48b392c6c7086f2fac942
blazewalletio <blazewallet.io@gmail.com>
2025-12-28 12:17:54 +0100

    Fix QuickPay JSX structure + subtle entrance animations

--- components/QuickPay.tsx
+++ components/QuickPay.tsx
@@ -329,6 +329,7 @@ export default function QuickPay() {
                         )}
                       </AnimatePresence>
                     </div>
+                    </div>
                   </div>
                 </div>
               </div>
